b9534d497cbade6cca205f2154b945bb6a76db35
Clark Alesna <clark_alesna@hotmail.com>
2023-12-14 09:38:24 +0800

    Example Yield Farming Chart

--- src/TeddySwap.UI/Components/Pages/Home.razor.cs
+++ src/TeddySwap.UI/Components/Pages/Home.razor.cs
@@ -14,13 +14,13 @@ public partial class Home
     #region Example
     private int Index = -1; //default value cannot be 0 -> first selectedindex is 0.
 
-    public List<ChartSeries> Series = new()
-    {
-        new ChartSeries() { Name = "United States", Data = [40, 20, 25, 27, 46, 60, 48, 80, 15] },
-        new ChartSeries() { Name = "Germany", Data = [19, 24, 35, 13, 28, 15, 13, 16, 31] },
-        new ChartSeries() { Name = "Sweden", Data = [8, 6, 11, 13, 4, 16, 10, 16, 18] },
-    };
-    public string[] XAxisLabels = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep"];
+    public List<ChartSeries> Series =
+    [
+        new ChartSeries() { Name = "Base Reward", Data = [40, 20, 25, 27, 46, 60, 48, 80, 15] },
+        new ChartSeries() { Name = "Bonus Reward", Data = [19, 24, 35, 13, 28, 15, 13, 16, 31] },
+        new ChartSeries() { Name = "Total Reward", Data = [8, 6, 11, 13, 4, 16, 10, 16, 18] },
+    ];
+    public string[] XAxisLabels = ["Dec 1", "Dec 2", "Dec 3", "Dec 4", "Dec 5", "Dec 6", "Dec 7", "Dec 8", "Dec 9"];
     #endregion
 
 
