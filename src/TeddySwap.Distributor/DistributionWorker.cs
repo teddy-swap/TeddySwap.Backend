3a21896a6ee08161b6d02f7179a00c974ab0fb16
Clark Alesna <clark_alesna@hotmail.com>
2023-12-20 02:05:37 +0800

    Update timeout for transaction confirmations

--- src/TeddySwap.Distributor/DistributionWorker.cs
+++ src/TeddySwap.Distributor/DistributionWorker.cs
@@ -273,7 +273,7 @@ public class DistributionWorker(
                     logger.LogInformation("Successfully submitted transaction {TxHash}", txHash);
                     await yieldFarmingDataService.SetYieldRewardByAddressClaimedAsync(processedYieldRewards, txHash);
                     await yieldFarmingDataService.SetYieldClaimRequestsProcessedAsync(processedClaimRequests, txHash, currentBlock.Number, currentBlock.Slot);
-                    if (await WaitTxConfirmationsWithTimeoutAsync(txHash, 2, 300))
+                    if (await WaitTxConfirmationsWithTimeoutAsync(txHash, 2, 5 * 60))
                     {
                         logger.LogInformation("Successfully confirmed transaction {TxHash}", txHash);
                     }
